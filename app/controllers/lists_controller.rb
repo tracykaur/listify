2571a9b38746d154728304096fc5012ed9fd6a47
tracykaur <tracykaur@yahoo.com>
2017-09-12 15:27:54 +1000

    packing page

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,6 +3,13 @@ class ListsController < ApplicationController
     @groceries = {
     	"Bread" => 2,
     	"Milk" => 2
-    }    
+    }
+  end
+
+  def packing
+    @clothes = {
+      "skirt" => 3,
+      "tops" => 4
+    }
   end
 end
